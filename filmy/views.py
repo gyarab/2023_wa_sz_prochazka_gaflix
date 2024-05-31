46451a17c68f5f2606aa876aa93ba1c70572de29
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-05-31 09:45:01 +0200

    prep for future

--- filmy/views.py
+++ filmy/views.py
@@ -6,3 +6,6 @@ from .models import Movie, Director, Actor, Genre
 def movies(request):
     context = {"movies": Movie.objects.all().order_by('name')}
     return render(request, "filmy/movies.html", context)
+#TODO - HOWGH
+def movie():
+    pass
